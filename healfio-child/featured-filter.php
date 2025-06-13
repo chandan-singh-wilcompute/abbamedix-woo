4bbce19427f4a4555e38481a1d290194c35ab55d
Ravindra Paliwal <ravindra884@gmail.com>
2025-06-13 12:17:59 +0530

    Done functionality of featured filter sorting

---
+++ healfio-child/featured-filter.php
@@ -0,0 +1,39 @@
+<?php
+	/**
+	 * Template Name: Product filter
+	 *
+	 * @package WordPress
+	 * @subpackage Healfio-child
+	 * @since Healfio 1.0
+	 */
+
+	get_header(); ?>
+
+<!-- <section class="woocommerce productFilterResultWrapper">
+		<?php //echo do_shortcode('[my_custom_filter]'); ?>
+
+		<?php //echo do_shortcode('[wc_ordering_dropdown]'); ?>
+
+		<?php //echo do_shortcode('[custom_product_filter_results]'); ?>
+</section> -->
+
+	<section class="woocommerce featuredFilterResultWrapper">
+		<?php echo do_shortcode('[my_custom_filter]');?>
+		
+		<?php // echo do_shortcode('[products paginate="true" columns="4" per_page="12"]');
+			echo do_shortcode('[custom_featured_filter_results]');
+		?>	
+
+	</section>
+
+
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/product-filter-dropdown.js"></script>
+	<script>
+		jQuery(document).ready(function ($) {
+			$('.productFilter').insertAfter('.titleWrapper');
+		});
+
+	</script>
+
+	<?php
+	get_footer();
